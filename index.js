920015d5392da292c3571366effa8a11bac15b66
samar <samarbahloul1@gmail.com>
2022-11-27 12:45:58 +0100

    add debug message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ new Promise(async (resolve, reject) => {
   try {
     response = await axios.get('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
       headers: {
-        'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
+        'X-CMC_PRO_API_KEY': process.env.MYAPIKEY,
       },
     });
   } catch(ex) {
